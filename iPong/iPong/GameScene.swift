0e96f4bfaace1eee3b062b06e1addc7213e9ce07
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-11 17:04:42 +0200

    MoveSet added, points in console
    player and enemy bars now have movement, player can move with the touch funcionality, ball now resets when a point is given. Score now visible in console

--- iPong/iPong/GameScene.swift
+++ iPong/iPong/GameScene.swift
@@ -15,14 +15,17 @@ class GameScene: SKScene {
     var myEnemy = SKSpriteNode()
     var myPlayer = SKSpriteNode()
     
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        startGame()
         
         myBall = self.childNode(withName: "spriteBall") as! SKSpriteNode
         myEnemy = self.childNode(withName: "spriteEnemy") as! SKSpriteNode
         myPlayer = self.childNode(withName: "spritePlayer") as! SKSpriteNode
         
         //Initial impulse
-        myBall.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
+        myBall.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
         
         //create the border arround the scene
         let myBorder = SKPhysicsBody(edgeLoopFrom: self.frame)
@@ -31,11 +34,63 @@ class GameScene: SKScene {
         
         self.physicsBody = myBorder
         
+    }
+    
+    func startGame() {
+        score = [0,0]
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        //reset the position and impulse of ball after each point
+        myBall.position = CGPoint(x: 0, y: 0)
+        myBall.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
         
+        if playerWhoWon == myPlayer {
+          score[0] += 1
+            myBall.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+
+        } else if playerWhoWon == myEnemy{
+            score[1] += 1
+            myBall.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+
+        }
+        
+        print(score)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        recognizeMovementForPlayerBar(touches)
+        
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        recognizeMovementForPlayerBar(touches)
     }
 
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        //Making the enemy move with the ball move but with a little delay
+        myEnemy.run(SKAction.moveTo(x: myBall.position.x, duration: 1.0))
+        
+        //testing positions for applying points
+        if myBall.position.y <= myPlayer.position.y - 70 {
+            addScore(playerWhoWon: myEnemy)
+            
+        } else if myBall.position.y >= myEnemy.position.y + 70 {
+            addScore(playerWhoWon: myPlayer)
+        }
+    }
+    
+    func recognizeMovementForPlayerBar(_ touches: Set<UITouch>){
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            myPlayer.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            
+
+            
+        }
     }
 }
